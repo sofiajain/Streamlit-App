4911c5996e7380fbdb9a7e3d2566a7d14942f3cf
Sofia Jain <sofiasjain@gmail.com>
2023-05-29 16:43:33 -0700

    Final Version

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from pandas.api.types import is_numeric_dtype
+from pandas.api.types import is_string_dtype
+from scipy.stats import gaussian_kde
+import matplotlib.pyplot as plt
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
+st.title('Stats 21 Discussion Project')
 
-st.title('title of application')
-st.markdown('here is a **bold** text')
-
-st.sidebar.title('sidebar')
-
-agree = st.checkbox('Click Me')
-if agree:
-    st.write('Great!')
-    st.markdown('this is *italic text*')
-
-side_check = st.sidebar.checkbox('Click This Box')
-if side_check:
-     st.sidebar.write('sidebar has been clicked')
-
-st.file_uploader('file uploader', 
+imported_file = st.file_uploader('file uploader', 
                  type=None, 
                  accept_multiple_files=False, 
                  key=None, 
@@ -25,3 +20,34 @@ st.file_uploader('file uploader',
                  kwargs=None,
                  disabled=False, 
                  label_visibility="visible")
+
+if imported_file is not None:   
+    file_data = pd.read_csv(imported_file)
+    st.write(file_data.head())
+    st.write('Rows:', file_data.shape[0])
+    st.write('Columns:' , file_data.shape[1])
+    st.write('Count for variable types:', file_data.dtypes.value_counts())
+
+    col_names = list(file_data.columns.values)
+    option = st.selectbox('Select Column', (col_names))
+
+    if(is_numeric_dtype(file_data[option])):
+        st.write('Summary Statistics')
+        st.write(file_data[option].describe())
+        sns.kdeplot(file_data[option], color='purple', fill=True, alpha=.3, linewidth=0)
+        st.pyplot()
+
+    if(is_string_dtype(file_data[option])):
+        st.write('Proportions of Each Category Level')
+        st.write(file_data[option].value_counts(normalize=True))
+        my_plot = sns.countplot(x=file_data[option], data=file_data)
+        my_plot.set_xticklabels(my_plot.get_xticklabels(), rotation=40, ha="right")
+        my_plot.set( ylabel='Count')
+        st.pyplot()
+
+
+
+    
+    
+
+
